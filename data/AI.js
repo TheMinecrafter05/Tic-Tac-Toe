23c52da961cb574692422938ca3592d4c8d6f00d
TheMinecrafter05 <59798742+TheMinecrafter05@users.noreply.github.com>
2020-01-28 19:58:25 +0100

    Update AI.js

--- data/AI.js
+++ data/AI.js
@@ -98,136 +98,6 @@ AI = setInterval(()=>{
     let button8 = document.getElementById("8")
     let button9 = document.getElementById("9")
 
-//if someone got 3
-if(button1.innerText == "O" && button2.innerText == "O" && button3.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button4.innerText == "O" && button5.innerText == "O" && button6.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button7.innerText == "O" && button8.innerText == "O" && button9.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button1.innerText == "O" && button4.innerText == "O" && button7.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button2.innerText == "O" && button5.innerText == "O" && button8.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button3.innerText == "O" && button6.innerText == "O" && button9.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button1.innerText == "O" && button2.innerText == "O" && button3.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button1.innerText == "O" && button5.innerText == "O" && button9.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button3.innerText == "O" && button5.innerText == "O" && button7.innerText == "O"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "AI hat gewonnen!"
-    won = true
-    return;
-}
-
-
-
-if(button1.innerText == "X" && button2.innerText == "X" && button3.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button4.innerText == "X" && button5.innerText == "X" && button6.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button7.innerText == "X" && button8.innerText == "X" && button9.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button1.innerText == "X" && button4.innerText == "X" && button7.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button2.innerText == "X" && button5.innerText == "X" && button8.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button3.innerText == "X" && button6.innerText == "X" && button9.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button1.innerText == "X" && button5.innerText == "X" && button9.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-if(button3.innerText == "X" && button5.innerText == "X" && button7.innerText == "X"){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Player hat gewonnen!"
-    won = true
-    return;
-}
-
-
-if(button1.innerText != "" && button2.innerText != "" && button3.innerText != "" && button4.innerText != "" && button5.innerText != "" && button6.innerText != "" && button7.innerText != "" && button8.innerText != "" && button9.innerText != ""){
-    clearInterval(AI)
-    document.getElementById("ansage").innerText = "Unentschieden!"
-    won = true
-    return;
-}
-
 
 
 if(turn == "player") return console.log("Player turn");
@@ -542,6 +412,14 @@ if(button1.innerText == "X" && button5.innerText == "X" && button9.innerText != 
                                                                                     turn = "player"
                                                                                     document.getElementById("ansage").innerText = "Your turn"
                                                                                     return;
+                                                                                }else{
+                                                                                    if(button1.innerText =="X" && button3.innerText =="X" && button2.innerText!="X"){
+                                                                                        button2.innerText = "O"
+                                                                                        console.log("logged button2")
+                                                                                        turn = "player"
+                                                                                        document.getElementById("ansage").innerText = "Your turn"
+                                                                                        return;
+                                                                                    }
                                                                                 }
                                                                             }
                                                                         }
@@ -639,4 +517,146 @@ if(button8.innerText == ""){
 }
 
 
-},1500)
+},2000)
+
+//if someone got 3
+setInterval(()=>{
+    let button1 = document.getElementById("1")
+    let button2 = document.getElementById("2")
+    let button3 = document.getElementById("3")
+    let button4 = document.getElementById("4")
+    let button5 = document.getElementById("5")
+    let button6 = document.getElementById("6")
+    let button7 = document.getElementById("7")
+    let button8 = document.getElementById("8")
+    let button9 = document.getElementById("9")
+
+    if(button1.innerText == "O" && button2.innerText == "O" && button3.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button4.innerText == "O" && button5.innerText == "O" && button6.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button7.innerText == "O" && button8.innerText == "O" && button9.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button1.innerText == "O" && button4.innerText == "O" && button7.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button2.innerText == "O" && button5.innerText == "O" && button8.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button3.innerText == "O" && button6.innerText == "O" && button9.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button1.innerText == "O" && button2.innerText == "O" && button3.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button1.innerText == "O" && button5.innerText == "O" && button9.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button3.innerText == "O" && button5.innerText == "O" && button7.innerText == "O"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "AI hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    
+    
+    if(button1.innerText == "X" && button2.innerText == "X" && button3.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button4.innerText == "X" && button5.innerText == "X" && button6.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button7.innerText == "X" && button8.innerText == "X" && button9.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button1.innerText == "X" && button4.innerText == "X" && button7.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button2.innerText == "X" && button5.innerText == "X" && button8.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button3.innerText == "X" && button6.innerText == "X" && button9.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button1.innerText == "X" && button5.innerText == "X" && button9.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    if(button3.innerText == "X" && button5.innerText == "X" && button7.innerText == "X"){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Player hat gewonnen!"
+        won = true
+        return;
+    }
+    
+    
+    if(button1.innerText != "" && button2.innerText != "" && button3.innerText != "" && button4.innerText != "" && button5.innerText != "" && button6.innerText != "" && button7.innerText != "" && button8.innerText != "" && button9.innerText != ""){
+        clearInterval(AI)
+        document.getElementById("ansage").innerText = "Unentschieden!"
+        won = true
+        return;
+    }
+},100)
